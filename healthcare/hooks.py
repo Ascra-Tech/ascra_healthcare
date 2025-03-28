e0fa604e205d3a9b8f22e9cd7bf1dfdbfa58b2e3
1byZero <amitkp66@live.com>
2025-03-28 15:00:00 +0530

    workspace update

--- healthcare/hooks.py
+++ healthcare/hooks.py
@@ -309,3 +309,22 @@ treeviews = [
 company_data_to_be_ignored = [
 	"Healthcare Service Unit",
 ]
+
+fixtures = [
+    #"Role",
+    #"Role Profile",
+    #"Client Script",
+    #"Server Script",
+    #"Tax Category",
+    "Workspace",
+    #"Custom HTML Block",
+    #"Custom DocPerm",
+    #"Workflow",
+    #"Workflow State",
+    #"Workflow Action",
+    #"Print Format",
+    #"Number Card",
+    #"Report",
+    #"Workflow Action Master",
+    #"Module Profile",
+]
