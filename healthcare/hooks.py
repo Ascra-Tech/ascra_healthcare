f285c6d275868f42d5243b0d624cfe329ff7a665
1byzero <55857959+1byZero@users.noreply.github.com>
2025-05-22 20:19:29 +0530

    patient discharge summary

--- healthcare/hooks.py
+++ healthcare/hooks.py
@@ -45,7 +45,8 @@ app_include_js = "healthcare.bundle.js"
 doctype_js = {
     "Sales Invoice": "public/js/sales_invoice.js",
     "Patient Appointment": "public/js/appointment_type_filters.js",
-    "Patient Appointment": "public/js/patient_appointment_history.js"
+    "Patient Appointment": "public/js/patient_appointment_history.js",
+    "Discharge Summary": "/public/js/discharge_summary_consumer_requests.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
